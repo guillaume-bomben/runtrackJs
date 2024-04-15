f8d169db5216f5c7046ffaae99381a66c90dc0d2
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-04-15 08:42:25 +0200

    Add function to calculate the sum of prime numbers in script.js

---
+++ jour01/job08/script.js
@@ -0,0 +1,20 @@
+function sommeNombresPremiers(number1,number2){
+    if (isPremier(number1) && isPremier(number2)){
+        return number1 + number2;
+    }
+    else{
+        return false;
+    }
+}
+
+function isPremier(number){
+    for (let i = 2; i < number; i++){
+        if (number % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(sommeNombresPremiers(3,7)); // 10
+console.log(sommeNombresPremiers(10,7)); // False
