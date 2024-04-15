b5aa783b884db76847b2c17e42801c3426272752
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-04-15 08:04:18 +0200

    Add function to display days of the week in script.js

---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+function afficherJoursSemaines(){
+    var jours_Semaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let i = 0; i < jours_Semaines.length; i++) {
+        console.log(jours_Semaines[i]);
+    }
+}
+
+afficherJoursSemaines();
